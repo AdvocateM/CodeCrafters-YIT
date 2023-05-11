c1a7993b7191b6de673f58b754ec05f93f4df8a6
Tresor Bipa Mabundi <66829445+TresorBipa@users.noreply.github.com>
2023-05-12 00:31:57 +0200

    merge change from main

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,3 @@
-// import { Route, Routes } from "react-router-dom";
-
-// import About from "./pages/About";
-// import Home from "./pages/Home";
-// import ForgotPassword from "./Auth/forgotPassword";
-// import Login from "./Auth/Login";
-// import ResetPassword from "./Auth/ResetPassword";
-// import TutorProfile from "./Profiles/TutorProfile";
-// import LearnerProfile from "./Profiles/LearnerProfile";
-
-
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-
-// const App = () => (
-	
-// 	<div>
-// 		<Routes>
-// 			<Route path="/" element={<Home />} />
-// 			<Route path="/about/this/site" element={<About />} />
-
-// 			{/*Forgot, login and reset PasswordRoutes test Mode */}
-// 			<Route path="/forgot-password" element={<ForgotPassword />} />
-// 			<Route path="/login" element={<Login />} />
-// 			<Route path="/reset_password/:id/:token" element={<ResetPassword />} />
-// 			{/* <Route path="/TutorProfile" element={<TutorProfile />} /> */}
-// 			<Route path="/tutors/:tutorId" element={<TutorProfile />} />
-// 			<Route path="/LearnerProfile" element={<LearnerProfile />} />
-// 		</Routes>
-// 		<ToastContainer />
-// 	</div>
-// );
-
-// export default App;
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
